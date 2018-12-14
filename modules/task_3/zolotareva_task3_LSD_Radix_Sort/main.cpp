cfead755ca5bb325c7da4cb5871f394366364de7
lesya89 <zolota98@mail.ru>
2018-12-14 11:45:19 +0300

    ver_13

--- modules/task_3/zolotareva_task3_LSD_Radix_Sort/main.cpp
+++ modules/task_3/zolotareva_task3_LSD_Radix_Sort/main.cpp
@@ -347,16 +347,16 @@ if (curr_rank_proc == ROOT) {
 
 MPI_Bcast(&size_arr, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
-    if (curr_rank_proc > 0)
+    if (curr_rank_proc > 0) {
       test_arr_pp_radix = new double[size_arr];
-    else {
-      std::cout<<"error";
-           break;
+    } else {
+      std::cout << "error";
+      std::cout << std::endl;
+           // break;
           }
 Calculate_work_and_displs(displs, send_num_work, size_arr);
 
        recv_buffer = new double[send_num_work[curr_rank_proc]];
-
 
 MPI_Scatterv(test_arr_pp_radix, send_num_work, displs,
            MPI_DOUBLE, recv_buffer, send_num_work[curr_rank_proc],
@@ -400,9 +400,6 @@ if (test_arr_pp_radix[i] != test_arr_seq_radix[i]) {
                 good = false;
                 break;
             }
-
-
-
 }
 MPI_Finalize();
 
